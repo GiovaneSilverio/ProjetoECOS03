f206190bd711e9867b13f2d5219dc8fcc6583297
Giovane <gcs000@hotmail.com>
2023-07-06 15:46:11 -0300

    Source code att
    Prirority-based Scheduling check;
    Round Robin Scheduling check;
    input archive check;
    main check.

--- code/main.c
+++ code/main.c
@@ -1,13 +1,8 @@
 #include "kernel.h"
 
-int main(void){
-    kernelInit();
-    kernelLoop();
-    /*
-    * segundo loop chamado apenas para demonstrar o funcionamento
-    * dos dois algoritmos. Em um sistema embarcado, o for(;;) dentro
-    * de kernelLoop() iria executar corretamente os dois algoritmos.
-    */
-    kernelLoop();
-    return 0;
+int main(void) {
+  kernelInit();
+  kernelLoop();  //execução do escalonador batch
+  kernelLoop();  //execução do escalonador interativo
+  return 0;
 }
